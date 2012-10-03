fb04e2b8acd25dcb869b96c46957673e5c2e6af9
Cam Spiers <camspiers@gmail.com>
2012-10-04 09:40:26 +1300

    UPDATE: Documentation and minor refactoring

--- code/HashPathExtension.php
+++ code/HashPathExtension.php
@@ -2,26 +2,54 @@
 
 class HashPathExtension extends Extension
 {
-
+    /**
+     * The format of the web path
+     * @var string
+     */
+    protected static $format = '%s/%s.v.%s.%s';
+    /**
+     * Returns an md5 hash of a file
+     * @param string  $path  Relative or absolute path to file
+     * @param boolean $theme Whether or not to take current theme into account
+     * @return string        The md5 of the file
+     */
     public function HashFile($path, $theme = true)
     {
         $path = $theme ? $this->getPath($path) : $path;
-
         return file_exists($path) ? md5_file($path) : '';
     }
-
+    /**
+     * Template function to return new web path to asset which includes
+     * md5 hash
+     * @param string  $path  Relative or absolute path to file
+     * @param boolean $theme Whether or not to take current theme into account
+     * @return string        The web path include the md5 hash
+     */
     public function HashPath($path, $theme = true)
     {
         $filepath = $this->getPath($path);
         $hash = $this->HashFile($filepath, false);
         $path_parts = pathinfo($filepath);
-
-        return str_replace(BASE_PATH, '', $path_parts['dirname']) . '/' . basename($path, ".{$path_parts['extension']}") . ".v.$hash.{$path_parts['extension']}";
+        return sprintf(
+            self::$format,
+            str_replace(BASE_PATH, '', $path_parts['dirname']),
+            basename($path, ".{$path_parts['extension']}"),
+            $hash,
+            $path_parts['extension']
+        );
     }
-
+    /**
+     * Returns the absolute path to a file based on whether or not
+     * the input is relative to the current theme
+     * @param  string  $path  Relative or absolute path to file
+     * @param  boolean $theme Whether or not to take current theme into account
+     * @return string         The absolute path to the file
+     */
     protected function getPath($path, $theme = true)
     {
-        return BASE_PATH . ($theme ? '/themes/' . SSViewer::current_theme() . "/$path" : $path);
+        return BASE_PATH . (
+            $theme ? '/themes/' . SSViewer::current_theme() . "/$path" : $path
+        );
     }
 
 }
