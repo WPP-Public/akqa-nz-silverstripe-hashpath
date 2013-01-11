723c71010f39126d13d2d434786f00c0259f7eff
Tim Higgs <tim.higgs@heyday.co.nz>
2013-01-11 15:29:17 +1300

    MINOR: Include SS autoloader for use in test cases

--- _config.php
+++ _config.php
@@ -1,5 +1,9 @@
 <?php
 
+if (file_exists(__DIR__ . '../vendor/autoload.php')) {
+    require_once __DIR__ . '../vendor/autoload.php';
+}
+
 define('HASH_PATH_RELATIVE_PATH', basename(dirname(__FILE__)));
 
 Object::add_extension('ContentController', 'HashPathExtension');
