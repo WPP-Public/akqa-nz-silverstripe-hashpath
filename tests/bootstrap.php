068df8ab67e53b892a1b8290a8ce084f3c52a36a
Stephen Holdaway <stecman@gmail.com>
2015-02-12 08:42:49 +1300

    Loosen composer/installers version requirement, remove unused dev dependencies
    Resolves #6 for the 1.0 branch

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,13 +1,13 @@
 <?php
 
-$dir = __DIR__ . '/..';
+$dir = dirname(__DIR__);
 
 if (file_exists($dir . '/vendor/autoload.php')) {
 
     $loader = require $dir . '/vendor/autoload.php';
 
-    $loader->addClassMap(Composer\Autoload\ClassMapGenerator::createMap($dir . '/framework'));
+    $loader->addClassMap(Composer\Autoload\ClassMapGenerator::createMap($dir . '/sapphire'));
     $loader->addClassMap(Composer\Autoload\ClassMapGenerator::createMap($dir . '/code'));
 }
 
-define('BASE_PATH', dirname(__DIR__));
+define('BASE_PATH', $dir);
