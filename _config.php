6baf564bc1d5028b0f7132fd13f0e82d3c895d3a
Ben Dubuisson <bendubuisson@users.noreply.github.com>
2017-07-13 16:27:00 +1200

    updated how we add the extension
    SS4 Beta has removed the Object class...

--- _config.php
+++ _config.php
@@ -5,5 +5,5 @@ if (file_exists(__DIR__ . '../vendor/autoload.php')) {
 }
 
 if (class_exists('SilverStripe\CMS\Controllers\ContentController')) {
-    \SilverStripe\Core\Object::add_extension('SilverStripe\CMS\Controllers\ContentController', 'Heyday\HashPath\HashPathExtension');
+    \SilverStripe\Core\Extensible::add_extension('SilverStripe\CMS\Controllers\ContentController', 'Heyday\HashPath\HashPathExtension');
 }
